819fdf145ce81b18ecdb1578ead6ded6dfd9d892
Buddy <huadrohne@gmail.com>
2025-04-26 15:41:00 +0200

    handlers.py aktualisieren

--- handlers.py
+++ handlers.py
@@ -1,8 +1,9 @@
+# Import handlers from separate files
 from handlers_base import start, reset_user_menu, send_branding
 from handlers_buttons import handle_buttons
-from handlers_conversations import handle_fahrer_name
 
+# Re-export everything for easier imports elsewhere
 __all__ = [
     'start', 'reset_user_menu', 'send_branding',
-    'handle_buttons',
-    'handle_fahrer_name'
+    'handle_buttons'
+]
